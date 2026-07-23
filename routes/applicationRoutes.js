e49ed1060db0044f669939195d59ef6dd0733e0e
Tshiva05 <webshiva23@gmail.com>
2026-07-23 20:53:09 +0530

    Add create-referral route to applicationRoutes

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -6,5 +6,9 @@ const router = express.Router();
 
 // GET /api/applications/track?mobile=9999999999
 router.get('/track', ctrl.trackByMobile);
+ 
+
+// POST /api/applications/:id/create-referral
+router.post('/:id/create-referral', ctrl.createReferral);
 
 module.exports = router;
