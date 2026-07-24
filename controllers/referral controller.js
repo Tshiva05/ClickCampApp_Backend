a994969aec0d605d66ae711bf35a27b03bbb5e69
Tshiva05 <webshiva23@gmail.com>
2026-07-24 17:32:53 +0530

    Create referral controller.js

---
+++ controllers/referral controller.js
@@ -0,0 +1,11 @@
+export const createReferral = async (req, res) => {
+   // Receive name, mobile, upi, offerSlug
+
+   // Find offer
+
+   // Generate referral code
+
+   // Save ReferralCreator
+
+   // Return referral link
+}
